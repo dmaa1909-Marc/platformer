9c2530ba056059df6ee40f82c493611d8994d08a
dmaa1909-Marc <66005851+dmaa1909-Marc@users.noreply.github.com>
2020-10-23 23:27:59 +0200

    Made movement and model changes.
    Changed the player model to an empty game object.
    Erased the old movement scripts.
    Began a new movement script.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # platformer
 Learning unity and using github.
+
+--Movement--
+Verical: W/S
+Horizontal: D/A
+
+Vertical and horizontal values are 1 / -1, based on which key is pressed, when no key is pressed the value is defaulted to 0.
+We will move the player using these inputs and multiplying them with a Vector3, which represents a direction in 3D space.
+This means that if W is pressed the vector has a size of one, if no key is pressed is has a size of zero and if S is pressed it has a size of -1.
+
+The vector's forwards/backwards/left/right-movement is based off the orientation of the camera, and the camera's up axis also rotates the parent object.
+This ensures the foward vector of the object always is facing the direction of the camera.
+If we did not implement this, it would result in a mess when the player would move- and look around.
+
+By using the getAxisRaw method we ensure to not have the player "sliding" when the wasd buttons is no longer pressed.
+In other words the value is instantly put at the value, when a butten is pressed or released.
+This is due to the regular getAxis method having a smoothness effect between transitioning between the values.
---
+++ Try Platformer/Assets/Script/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+    //Serializefield makes a private variable visible in the inspector (much more complicated, but that's the short version)
+    [SerializeField] Transform playerCamera = null;
+    [SerializeField] float mouseSensitivity = 3.5f;
+    [SerializeField] float walkSpeed = 6.0f;
+
+    [SerializeField] bool lockCursor = true;
+
+    private float cameraPitch = 0.0f;
+    private CharacterController controller = null;
+
+    // Start is called before the first frame update
+    void Start(){
+
+        controller = GetComponent<CharacterController>();
+
+        //If lockCursor is true it locks the cursor to the center of the screen and makes it invisible
+        if (lockCursor) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update(){
+        MouseLookUpdate();
+        MovementUpdate();
+    }
+
+
+    private void MouseLookUpdate() {
+        //Returns the axis of the mouse placement
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        //Inverts the camera axis value (unity camera is defaulted to "look upwards" with a negative value)
+        cameraPitch -= mouseDelta.y * mouseSensitivity;
+
+        //Clamps the axis of the camera (meaning to lock it in place) if the min/max axis value is recieved.
+        //Makes it so the camera cant rotate more than 90 degrees up or down.
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+
+        //Rotates the camera up and down, based on the mouse's y position
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        //Rotates the player left and right, based on mouse x position
+        transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
+
+    }
+
+    private void MovementUpdate() {
+        //Returns the input of a movement-keypress
+        Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        //Ensures the value when moving diagonally also is 1, to ease implementation of a movement speed and make it so the speed dosen't increase when moving sideways.
+        inputDir.Normalize();
+
+        //Moves the player object based on the value of which movementkeys are pressed.
+        Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x) * walkSpeed;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
